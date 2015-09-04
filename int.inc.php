323eb6844b9960fbd03a1fd801ce20dbd997ffdf
Michael <keke231@qq.com>
2015-09-04 16:17:08 +0800

    use composer autoload

--- int.inc.php
+++ int.inc.php
@@ -2,6 +2,7 @@
 define('BASE_DIR',  __DIR__ . '/');
 define('CONFIG_DIR', BASE_DIR . 'Config');
 
-require BASE_DIR . 'Tiny/Loader/Loader.php';
-spl_autoload_register('\\Tiny\\Loader\\Loader::load');
+//require BASE_DIR . 'Tiny/Loader/Loader.php';
+//spl_autoload_register('\\Tiny\\Loader\\Loader::load');
+require 'vendor/autoload.php';
 
